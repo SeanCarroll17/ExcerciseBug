a54b6c6fd69833168c93f83a3d7b580b3b3aa1b0
SeanCarroll17 <31080468+SeanCarroll17@users.noreply.github.com>
2017-11-08 02:53:29 +0000

    Update and rename Bug.js to fixed.js

--- fixed.js
+++ fixed.js
@@ -1,14 +1,12 @@
 function Kid(kid, name){
-  this.kid = false;
+  this.kid = kid;
   this.name = name;
 }
  
-Kid.prototype.toString = function(){
-	var convert = '';
+Object.prototype.toString = function(){
     	for(var all in this){
-            convert += all + ' ' + this[all];
+            console.log(` ${all} : ${this[all]} `);
         }
-		return convert;
 }
 
 var newChild = new Kid(false, 'sean')
